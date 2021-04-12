d43811c0b9cfa3e0388f525d55a1592693dacad2
Jafar <mohamad.jafar17016@student.unsika.ac.id>
2021-04-12 19:17:12 +0700

    first commit

---
+++ lib/extensions/extensions.dart
@@ -0,0 +1,6 @@
+import 'package:flutix/models/models.dart';
+import 'package:flutix/services/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+part 'firebase_user_extension.dart';
+part 'date_time_extension.dart';
---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+String apiKey = "d4be356f72118bf172ffd9d32c742a53";
+String imageBaseURL = "https://image.tmdb.org/t/p/";
+ 
+PageEvent prevPageEvent;
+File imageFileToUpload;
